863aa21b39fcd07308995bf244e1c804e92a1689
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-03-10 20:51:13 +0200

    feat: create search widget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -13,12 +13,36 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: brandName(),
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
-
+      body: Container(
+          child: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Color(0xfff5f8fd),
+                borderRadius: BorderRadius.circular(30)),
+            margin: EdgeInsets.symmetric(horizontal: 24),
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "search wallpapers",
+                        border: InputBorder.none),
+                  ),
+                ),
+                Icon(Icons.search)
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
