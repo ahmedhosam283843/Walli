b662716d2596836925683c6790b9a089f4092747
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-03-14 19:54:52 +0200

    feat: display wallpapers on main screen

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -27,6 +27,10 @@ class _HomeState extends State<Home> {
       PhotosModel photosModel = PhotosModel.fromMap(element);
       wallpapers.add(photosModel);
     });
+
+    setState(() {
+      wallpapers = wallpapers;
+    });
   }
 
   @override
@@ -45,48 +49,54 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.white,
         elevation: 0.0,
       ),
-      body: Container(
-          child: Column(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                color: Color(0xfff5f8fd),
-                borderRadius: BorderRadius.circular(30)),
-            margin: EdgeInsets.symmetric(horizontal: 24),
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "search wallpapers",
-                        border: InputBorder.none),
-                  ),
-                ),
-                Icon(Icons.search)
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Container(
-            height: 80,
-            child: ListView.builder(
+      body: SingleChildScrollView(
+        child: Container(
+            child: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xfff5f8fd),
+                  borderRadius: BorderRadius.circular(30)),
+              margin: EdgeInsets.symmetric(horizontal: 24),
               padding: EdgeInsets.symmetric(horizontal: 24),
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              itemCount: categories.length,
-              itemBuilder: (context, index) {
-                return CategorieWidget(
-                  title: categories[index].categorieName,
-                  imgURL: categories[index].imgUrl,
-                );
-              },
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "search wallpapers",
+                          border: InputBorder.none),
+                    ),
+                  ),
+                  Icon(Icons.search)
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              height: 80,
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 24),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  return CategorieWidget(
+                    title: categories[index].categorieName,
+                    imgURL: categories[index].imgUrl,
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            wallPaper(wallpapers, context)
+          ],
+        )),
+      ),
     );
   }
 }
@@ -110,7 +120,8 @@ class CategorieWidget extends StatelessWidget {
             ),
           ),
           Container(
-            color: Colors.black26,
+            decoration: BoxDecoration(
+                color: Colors.black26, borderRadius: BorderRadius.circular(8)),
             height: 50,
             width: 100,
             alignment: Alignment.center,
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,6 +2,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../models/wallpaper_model.dart';
+
 Widget brandName() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
@@ -13,5 +15,29 @@ Widget brandName() {
             // fontWeight: FontWeight.bold,
           ))
     ],
+  );
+}
+
+Widget wallPaper(List<PhotosModel> listPhotos, BuildContext context) {
+  return Container(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    child: GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: 0.6,
+        physics: const ClampingScrollPhysics(),
+        shrinkWrap: true,
+        mainAxisSpacing: 6.0,
+        crossAxisSpacing: 6.0,
+        children: listPhotos.map((wallpaper) {
+          return GridTile(
+              child: Container(
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(16),
+                child: Image.network(
+                  wallpaper.src.portrait,
+                  fit: BoxFit.cover,
+                )),
+          ));
+        }).toList()),
   );
 }
